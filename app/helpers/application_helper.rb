6d2143af1d6dc6c96b658a4dc5038d303c8446e9
DanielAlk <daniodfad@yahoo.com.ar>
2016-03-29 06:47:06 -0300

    bunch of stuff you do at the begining

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+	def head_allow_robots?
+		true
+	end
+
+	def head_title
+		case params['action']
+		when 'home'
+			'Beigel'
+		else
+			'Beigel'
+		end
+	end
+
+	def head_description
+		''
+	end
+
+	def head_og_image
+		''
+	end
+
+	def nav_class(action = nil)
+		if action.kind_of? Array
+			'active' if action.include? params['action']
+		else
+			'active' if action == params['action']
+		end
+	end
 end
