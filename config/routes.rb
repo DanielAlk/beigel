1d1d12e4b595e93841188094d4ad93e3054f2ed2
DanielAlk <daniodfad@yahoo.com.ar>
2016-04-01 04:44:15 -0300

    buy and rent maquetado

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
   get 'emprendimientos' => 'pages#developments', as: :developments
+  get 'comprar' => 'pages#buy', as: :buy
+  get 'alquilar' => 'pages#rent', as: :rent
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
