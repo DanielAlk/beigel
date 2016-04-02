86117aec7e367ec8ba73fd69f7f6b489161871cb
DanielAlk <daniodfad@yahoo.com.ar>
2016-04-02 04:22:23 -0300

    file maquetado

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,5 @@
 module PagesHelper
 	def currency(number, unit)
-		number_to_currency(number, delimiter: '.', separator: ',', precision: 0, unit: unit + ' ') || '-'
+		number_to_currency(number, delimiter: '.', separator: ',', precision: 0, unit: unit) || '-'
 	end
 end
